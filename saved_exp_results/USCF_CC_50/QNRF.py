0cab11d9f6e615a08845e9156f4e5fc4b5366724
avibest1 <avinashrohra5@gmail.com>
2025-09-15 18:48:53 +0800

    Add files via upload

---
+++ saved_exp_results/USCF_CC_50/QNRF.py
@@ -0,0 +1,23 @@
+from easydict import EasyDict as edict
+
+
+# init
+__C_USCF_CC_50 = edict()
+
+cfg_data = __C_USCF_CC_50
+
+__C_USCF_CC_50.TRAIN_SIZE = (512,1024)
+__C_USCF_CC_50.DATA_PATH = '../ProcessedData/USCF_CC_50/'
+__C_USCF_CC_50.TRAIN_LST = 'train.txt'
+__C_USCF_CC_50.VAL_LST =  'val.txt'
+__C_USCF_CC_50.VAL4EVAL = 'val_gt_loc.txt'
+
+__C_USCF_CC_50.MEAN_STD = ([0.413525998592, 0.378520160913, 0.371616870165], [0.284849464893, 0.277046442032, 0.281509846449])
+
+__C_USCF_CC_50.LABEL_FACTOR = 1
+__C_USCF_CC_50.LOG_PARA = 1.
+
+__C_USCF_CC_50.RESUME_MODEL = ''#model path
+__C_USCF_CC_50.TRAIN_BATCH_SIZE = 6 #imgs
+
+__C_USCF_CC_50.VAL_BATCH_SIZE = 1 # must be 1
